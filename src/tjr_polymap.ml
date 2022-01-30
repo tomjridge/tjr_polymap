0dec9fb8d96263bf397993db1f64938fd9bf854d
Tom Ridge <tom.j.ridge@googlemail.com>
2022-01-30 11:42:07 +0000

    +

--- src/tjr_polymap.ml
+++ src/tjr_polymap.ml
@@ -72,9 +72,9 @@ let foldi: ('a -> 'b -> 'c -> 'c) -> ('a , 'b) t -> 'c -> 'c =
   fun f t c ->
     PMap.foldi f t.map c
 
-let empty_int_map () = empty (Pervasives.compare : int -> int -> int)
+let empty_int_map () = empty (Stdlib.compare : int -> int -> int)
 
-let empty_string_map () = empty (Pervasives.compare : string -> string -> int)
+let empty_string_map () = empty (Stdlib.compare : string -> string -> int)
 
 (** NOTE later bindings take precedence *)
 let from_bindings ~compare kvs =
